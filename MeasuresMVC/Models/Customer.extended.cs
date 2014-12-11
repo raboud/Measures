19e0bc43f2f97a39db4f28ff200781dda5949b63
Robert Raboud <robert.raboud@outlook.com>
2014-12-11 18:22:45 +0000

    Changes to Chargeback
    Originally checked in by TFSSERVER\rraboud on 2014-01-16 07:34:43.373 as Changeset 1244
    
    git-tfs-id: [https://randr.visualstudio.com]$/Measures;C3002

---
+++ MeasuresMVC/Models/Customer.extended.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeasuresMVC.Models
+{
+	[MetadataType(typeof(Customer.MetaData))]
+	public partial class Customer
+	{
+		public class MetaData
+		{
+			[Display(Name = "Last Name", Prompt = "Enter Last Name", Description="Customer Last Name")]
+			[StringLength(50)]
+			public string LastName;
+
+			[Display(Name = "First Name", Prompt = "Enter First Name", Description="Customer First Name")]
+			[StringLength(50)]
+			public string FirstName;
+
+			[DataType(DataType.EmailAddress)]
+			public string EmailAddress;
+
+		}
+	}
+
+}
