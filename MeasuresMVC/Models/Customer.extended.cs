f67b5087467d1ec9c1df0a9447e9ee32e9a3027d
Robert Raboud <robert.raboud@outlook.com>
2014-12-11 18:23:04 +0000

    Setup Customer.extended.cs Model to reflect textboxes - made some required
    'Create Measure' button on Home page opens to Customer Info page
    
    Placed CFI image as Home link in Navigation bar, but it is not displaying properly.
    
    Originally checked in by TFSSERVER\jraboud on 2014-01-16 16:59:44.95 as Changeset 1246
    
    git-tfs-id: [https://randr.visualstudio.com]$/Measures;C3005

--- MeasuresMVC/Models/Customer.extended.cs
+++ MeasuresMVC/Models/Customer.extended.cs
@@ -11,16 +11,57 @@ namespace MeasuresMVC.Models
 	{
 		public class MetaData
 		{
-			[Display(Name = "Last Name", Prompt = "Enter Last Name", Description="Customer Last Name")]
-			[StringLength(50)]
+            [Display(Name = "First Name*", Prompt = "Enter First Name", Description = "Customer First Name")]
+            [StringLength(20)]
+            [Required]
+            public string FirstName;
+
+			[Display(Name = "Last Name*", Prompt = "Enter Last Name", Description="Customer Last Name")]
+			[StringLength(20)]
+            [Required]
 			public string LastName;
 
-			[Display(Name = "First Name", Prompt = "Enter First Name", Description="Customer First Name")]
-			[StringLength(50)]
-			public string FirstName;
-
 			[DataType(DataType.EmailAddress)]
+            [Display(Name = "Email Address*", Prompt = "Enter Customer Email Address", Description = "Customer Email")]
+            [Required]
 			public string EmailAddress;
+
+            [Display(Name = "Address*", Prompt = "Enter Customer Address", Description = "Customer Address Line 1")]
+            [StringLength(50)]
+            [Required]
+            public string Address;
+
+            [Display(Name = "Address Additional", Prompt = "Enter Customer Address", Description = "Customer Address Line 2")]
+            [StringLength(50)]
+            public string Address2;
+
+            [Display(Name = "City*", Prompt = "Enter City", Description = "Customer City")]
+            [StringLength(30)]
+            [Required]
+            public string City;
+
+            [Display(Name = "State*", Prompt = "Enter State", Description = "Customer State")]
+            [StringLength(2)]
+            [Required]
+            public string State;
+
+            [Display(Name = "Zip Code*", Prompt = "Enter Zip Code", Description = "Customer Zip Code")]
+            [StringLength(5)]
+            [Required]
+            public string ZipCode;
+
+            [Display(Name = "Home Number", Prompt = "Enter Home Phone Number", Description = "Customer Home Phone")]
+            [StringLength(10)]
+            public string PhoneNumber;
+
+            [Display(Name = "Mobile Number", Prompt = "Enter Mobile Phone Number", Description = "Customer Mobile Phone")]
+            [StringLength(10)]
+            public string MobileNumber;
+
+            [Display(Name = "Work Number", Prompt = "Enter Work Number", Description = "Customer Work Number")]
+            [StringLength(10)]
+            public string WorkNumber;
+
 
 		}
 	}
